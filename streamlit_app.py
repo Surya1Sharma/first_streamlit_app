bc1b13f76d78d21101a0e8022fc58f6ceceeb352
Surya1Sharma <144148910+Surya1Sharma@users.noreply.github.com>
2023-09-05 11:05:30 -0500

    Update streamlit_app.py
    Fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title("My Parents New Healty Diner")
+streamlit.header('Breakfast Menue')
+streamlit.text('Omega3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('hard Boiled & Free-Range Egg')
